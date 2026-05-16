a61bed9bec263b43c35ed5a2654ff20172149cfb
Anderson Moreton <anderson.moreton@gmail.com>
2026-05-16 11:51:04 +0100

    Configurar o backend com Docker e MySQL

---
+++ opa-backend/src/database/connection.ts
@@ -0,0 +1,19 @@
+import mysql from 'mysql2/promise';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const pool = mysql.createPool({
+
+  host: process.env.DB_HOST || 'localhost',
+
+  port: Number(process.env.DB_PORT) || 3307,
+
+  user: process.env.DB_USER || 'root',
+
+  password: process.env.DB_PASSWORD || 'root',
+
+  database: process.env.DB_NAME || 'opa_db_dev'
+
+});
