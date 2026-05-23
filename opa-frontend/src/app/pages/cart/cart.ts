a39a582f7883f3f9155d6ab21bea2a6fb6ac7d60
Anderson Moreton <anderson.moreton@gmail.com>
2026-05-23 22:59:35 +0100

    feat: adiciona validação de login antes de finalizar pedido

--- opa-frontend/src/app/pages/cart/cart.ts
+++ opa-frontend/src/app/pages/cart/cart.ts
@@ -1,23 +1,54 @@
 import { Component } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+
+import { Router, RouterLink } from '@angular/router';
+
 import { HeaderComponent } from '../../components/header/header';
+
 import { FooterComponent } from '../../components/footer/footer';
+
 import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent, RouterLink],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    RouterLink
+  ],
   templateUrl: './cart.html',
-  styleUrls: ['./cart.css'],
+  styleUrls: ['./cart.css']
 })
+
 export class CartComponent {
 
-  constructor(public cartService: CartService) {}
-  checkout() {
-    alert('Pedido finalizado!');
-    this.cartService.clearCart();
+  constructor(
+    public cartService: CartService,
+    private router: Router
+  ) {}
+
+  checkout(): void {
+
+    const user =
+      localStorage.getItem('user');
+
+    if (!user) {
+
+      this.router.navigate([
+        '/login'
+      ]);
+
+      return;
+
+    }
+
+    this.router.navigate([
+      '/payment'
+    ]);
+
   }
 
 }
