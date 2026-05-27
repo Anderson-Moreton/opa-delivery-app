b859ae04939c111e73dcd613d67456d1f986f481
Anderson Moreton <anderson.moreton@gmail.com>
2026-05-27 18:38:11 +0100

    feat: implementa fluxo completo de autenticação e menu do usuário

--- opa-frontend/src/app/pages/login/login.ts
+++ opa-frontend/src/app/pages/login/login.ts
@@ -1,32 +1,56 @@
 import { Component } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+
+import { Router, RouterModule } from '@angular/router';
 
 import { HeaderComponent } from '../../components/header/header';
+
 import { FooterComponent } from '../../components/footer/footer';
+
+import { AuthService } from '../../services/auth.service';
+
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule,
-    HeaderComponent,
-    FooterComponent
-  ],
+  imports: [CommonModule, FormsModule, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './login.html',
-  styleUrls: ['./login.css']
+  styleUrls: ['./login.css'],
 })
 export class LoginComponent {
+  email = '';
 
-  email: string = '';
-  password: string = '';
+  password = '';
 
-  login() {
-    console.log('Email:', this.email);
-    console.log('Senha:', this.password);
+  constructor(
+    private authService: AuthService,
+    private toast: ToastService,
+    private router: Router,
+  ) {}
+
+  login(): void {
+    const credentials = {
+      email: this.email,
+
+      password: this.password,
+    };
+
+    this.authService.login(credentials).subscribe({
+      next: (user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+
+        this.toast.show('Login successful', 'success');
+
+        this.router.navigate(['/']);
+      },
+
+      error: (error) => {
+        this.toast.show(error.error.message || 'Login error', 'error');
+      },
+    });
   }
-
 }
